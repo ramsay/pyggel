0d041a15c86f3317ba8d7262734caeba2271ebf5
RoeBros <RoeBros@29c87561-914b-0410-992d-b37967ad6221>
2009-03-24 23:03:10 +0000

    TAG for release version 0.07-alpha3

---
+++ tags/v0.07/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(name='pyggel',
+      version='0.06-alpha2',
+      description='PYthon Graphical Game Engine and Libraries',
+      author='Matt Roe',
+      author_email='RoeBros@gmail.com',
+      maintainer='Robert Ramsay',
+      maintainer_email='durandal@gmail.com',
+      url='http://pyggel.googlecode.com/',
+      packages = ['pyggel'],
+      #TODO: the demos and 3d files
+      #data_files=[('data', ['data/*.png']),
+      #            ('data', ['data/*.obj']),
+      #            ('data', ['data/*.mtl']),]
+      )
